d64396cb2bd3ed1213bacd20b98c870495fa1e5f
patricia surf <74924633+patsurf@users.noreply.github.com>
2022-04-10 06:23:19 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,29 @@
 import os
+import sys
 from dotenv import load_dotenv
 
 load_dotenv()  # load environment variables from .env if it exists.
+
+EXEC_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    UPLOAD_FOLDER = UPLOAD_FOLDER = "./uploads"
+    UPLOAD_FOLDER = UPLOAD_FOLDER = "./Uploads"
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     # SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    
+
+     # Create the upload folder if its deleted
+    if os.path.isdir(UPLOAD_FOLDER) is False:
+        os.mkdir(UPLOAD_FOLDER)
+
+class DevelopmentConfig(Config):
+    """Development Config that extends the Base Config Object"""
+    DEVELOPMENT = True
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Production Config that extends the Base Config Object"""
+    DEBUG = False
