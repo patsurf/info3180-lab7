dfe3371a7c9f54ebdfbe1b6484a704d9f28ec677
patricia surf <74924633+patsurf@users.noreply.github.com>
2022-04-10 05:52:08 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -8,6 +8,9 @@ This file creates your application.
 from app import app
 from flask import render_template, request, jsonify, send_file
 import os
+from app.forms import UploadForm
+from flask_wtf.csrf import generate_csrf
+from werkzeug.utils import secure_filename
 
 
 ###
@@ -19,24 +22,44 @@ def index():
     return jsonify(message="This is the beginning of our API")
 
 
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    form = UploadForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        description = request.form['description']
+        photo = request.files['photo']
+        filename = secure_filename(photo.filename)
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify({'message': "File Upload Successful", 'description': description, 'filename': filename }),200 
+    else:
+        return jsonify({'errors': form_errors(form)}), 400
+
+
+@app.route('/api/csrf-token', methods=['GET'])
+def get_csrf():
+    return jsonify({'csrf_token': generate_csrf()})
+
 ###
 # The functions below should be applicable to all Flask apps.
 ###
 
 # Here we define a function to collect form errors from Flask-WTF
 # which we can later use
+
+
 def form_errors(form):
     error_messages = []
     """Collects form errors"""
     for field, errors in form.errors.items():
         for error in errors:
             message = u"Error in the %s field - %s" % (
-                    getattr(form, field).label.text,
-                    error
-                )
+                getattr(form, field).label.text,
+                error
+            )
             error_messages.append(message)
 
     return error_messages
+
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
@@ -64,4 +87,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port="8080")
+    app.run(debug=True, host="0.0.0.0", port="8080")
