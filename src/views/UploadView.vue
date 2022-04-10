1f2923fc043e8b4301e47e98775ba47fe28b5b7e
patricia surf <74924633+patsurf@users.noreply.github.com>
2022-04-10 05:59:24 -0500

    Update UploadView.vue

---
+++ src/views/UploadView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <h2>Upload</h2>
+        <UploadForm/>
+    </div>
+</template>
+
+<script>
+import UploadForm from "@/components/UploadForm.vue";
+export default {
+    data() {
+        return {}
+    },
+    components: {
+        UploadForm
+    }
+};
+</script>
