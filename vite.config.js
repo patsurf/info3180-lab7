a52dd9e4e743b4607c6c01c323a86a9262bc3d0c
patricia surf <74924633+patsurf@users.noreply.github.com>
2022-04-10 06:11:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { fileURLToPath, URL } from 'url'
+import {
+  fileURLToPath,
+  URL
+} from 'url'
 
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -8,7 +13,15 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src',
+        import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '^/api*': {
+        target: 'http://localhost:8080/'
+      }
     }
   }
 })
